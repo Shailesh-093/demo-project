48fe7c1ff6fcd0c649e013b007e087072127c307
Ankush Patil <hackertech1510@gmail.com>
2025-07-26 15:47:53 +0530

    hello guys i have added JS file

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+  // Show a welcome message when the page loads
+  window.onload = function() {
+    alert("Welcome to Sommer's Portfolio Page!");
+    console.log("Page loaded successfully.");
+  };
