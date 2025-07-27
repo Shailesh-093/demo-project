b443283c248142ac739a58a104fc7301db2f07b8
Ankush Patil <hackertech1510@gmail.com>
2025-07-27 06:25:12 +0530

    hey guys i have made chnages in Main branch

---
+++ script.js
@@ -0,0 +1,17 @@
+// Scroll animation
+const sections = document.querySelectorAll('.section');
+
+function revealOnScroll() {
+  const triggerBottom = window.innerHeight * 0.8;
+
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+
+    if (top < triggerBottom) {
+      section.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
